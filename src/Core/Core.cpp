21c8564e0960434ae994f9782913a7ae018f91d2
Yom <tom.laiolo@epitech.eu>
2023-03-25 18:09:34 +0100

    ADD personnage qui bouge

--- src/Core/Core.cpp
+++ src/Core/Core.cpp
@@ -7,7 +7,7 @@
 
 #include "Core.hpp"
 
-Game::Core::Core(std::string option) : _parallax(), _player1("assets/player1.png"), _player2("assets/player2.png"), _map("uwu"), _window(sf::VideoMode(1920, 1080), "JAM_paralel"), _clock(), _time()
+Game::Core::Core(std::string option) : _parallax(), _player1("assets/player1.png", {850, 368}), _player2("assets/player2.png", {850, 710}), _map(option), _window(sf::VideoMode(1920, 1080), "JAM_paralel"), _clock(), _time()
 {
     _dtime = 0;
     _clock.restart();
@@ -21,15 +21,22 @@ Game::Core::~Core()
 
 void Game::Core::Run()
 {
+    _player2.setScale({1, -1});
     while (_window.isOpen()) {
         _dtime = getDtime();
         sf::Event event;
         while (_window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
+            if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
                 _window.close();
+                return;
+            }
         }
         _window.clear();
         _parallax.update(_window, _dtime);
+        _player1.update(_dtime);
+        _player2.update(_dtime);
+        _player1.draw(_window);
+        _player2.draw(_window);
         _window.display();
     }
 }
